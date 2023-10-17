bca9ee6d7065adc02eb0b8c6146ad1c6561c9ddc
blackspy03 <albertchrishansenxd@gmail.com>
2023-10-17 19:48:20 +0700

    minggu7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: "admin", component: AdminComponent},
   {path: "login", component: LoginComponent},
+  {path: "dashboard", component: DashboardComponent},
+  {path: "dashboard2", component: Dashboard2Component},
+  {path: "dashboard3", component: Dashboard3Component},
   {path: "register", component: RegisterComponent},
   {path: "", redirectTo: "login", pathMatch:"full"},
 ];
