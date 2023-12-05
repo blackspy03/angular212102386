9f028a2abd5f2232824932f7ca1681c1f7e3bfc5
rangga-S <127275891+Separianusyogi@users.noreply.github.com>
2023-12-05 19:24:33 +0700

    minggu12
    kelar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,20 +7,33 @@ import { Dashboard2Component } from './dashboard2/dashboard2.component';
 import { Dashboard3Component } from './dashboard3/dashboard3.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+import { otentikasiGuard } from './otentikasi.guard';
 
 const routes: Routes = [
-  {path: "admin", component: AdminComponent},
-  {path: "login", component: LoginComponent},
-  {path: "dashboard", component: DashboardComponent},
-  {path: "dashboard2", component: Dashboard2Component},
-  {path: "dashboard3", component: Dashboard3Component},
-  {path: "register", component: RegisterComponent},
-  {path: "mahasiswa", component: MahasiswaComponent},
-  {path: "", redirectTo: "login", pathMatch:"full"},
+  { path: 'admin', component: AdminComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [otentikasiGuard],
+  },
+  {
+    path: 'dashboard2',
+    component: Dashboard2Component,
+    canActivate: [otentikasiGuard],
+  },
+  { path: 'dashboard3', component: Dashboard3Component },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'mahasiswa',
+    component: MahasiswaComponent,
+    canActivate: [otentikasiGuard],
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,58 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+declare const $: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-
+export class LoginComponent implements OnInit {
   nama = 'Albert Chrishansen';
   nim = '212102386';
-  constructor(private renderer: Renderer2){
-    this.renderer.addClass(document.body, "login-page");
+
+  constructor(private router: Router, private http: HttpClient) {}
+
+  showPeringatanModal(message: string): void {
+    $('#peringatanModal').modal();
+    $('#pm_message').html(message);
   }
 
-  ngOnInit(): void{
+  ngOnInit(): void {}
 
+  signIn(): void {
+    console.log('signIn()');
+
+    var userId = $('#idText').val();
+    userId = encodeURIComponent(userId);
+
+    var password = $('#passwordText').val();
+    password = encodeURIComponent(password);
+
+    var url =
+      'https://stmikpontianak.net/011100862/login.php' +
+      '?id=' +
+      userId +
+      '&password=' +
+      password;
+
+    console.log('url : ' + url);
+
+    this.http.get(url).subscribe((data: any) => {
+      console.log(data);
+
+      var row = data[0];
+
+      if (row.idCount != '1') {
+        this.showPeringatanModal('Id atau password tidak cocok');
+        return;
+      }
+
+      sessionStorage.setItem('userId', userId);
+      console.log('session data berhasil dibuat');
+      this.router.navigate(['/dashboard']);
+    });
   }
 }
